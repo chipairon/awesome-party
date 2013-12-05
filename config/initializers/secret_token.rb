cce9ea9002c29e47d902b94f8e98ed921062ba34
Chipairon <chipairon@fake.server>
2013-12-05 11:02:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Naranjas::Application.config.secret_token = '0454a90c770f9795eee35f42714a6c5c4e277ea7c2fdcd15182d3fec642963a8350c9d70d67c7661dbe0e0cbfccbb81fe1feb6fbddd8ef9215eb7b1506fa04ca'
